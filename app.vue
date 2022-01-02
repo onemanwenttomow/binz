363d879cf8109b08882b8b35193dc6d3bc0c0ac0
Peter Anderson <pete.anderson@gmail.com>
2022-01-02 14:02:46 +0100

    --add: started calendar view

--- app.vue
+++ app.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
 const { data: binzData } = await useAsyncData('binz', () => $fetch('/api/binz'));
+const isMounted = ref(false);
+onMounted(() => {
+  isMounted.value = true;
+});
 </script>
 
 <template>
-  <div v-for="binz in binzData" :key="binz.date">
-    {{ binz.bin }} - {{ binz.day }} {{ binz.date }}
+  <div v-if="isMounted">
+    <v-calendar is-expanded :attributes="binzData" />
   </div>
+  <pre>{{ binzData }}</pre>
 </template>
--- server/api/binz.ts
+++ server/api/binz.ts
@@ -1,4 +1,22 @@
 import data from '../2022/recycling-schedule.json';
 
+// There are 8 pre-defined color sets available (gray, red, orange, yellow, green, teal, blue, indigo, purple, pink).
+
+const cols = {
+  'Gelbe Tonne': 'yellow',
+  Biotonne: 'green',
+  Reststoff: 'gray',
+  'Blaue Tonne': 'blue',
+};
+
 const binzData: BinDay[] = data;
-export default () => binzData;
+export default () =>
+  binzData.map((binDay, i) => {
+    const dmy = binDay.date.split('.');
+    const d = new Date(Number(dmy[2]), Number(dmy[1]) - 1, Number(dmy[0]) + 1);
+    return {
+      key: i,
+      highlight: cols[binDay.bin],
+      dates: d,
+    };
+  });
