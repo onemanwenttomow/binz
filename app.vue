558180f868f9c856c9b207a1ff72bf10309d68fa
Peter Anderson <pete.anderson@gmail.com>
2022-01-02 17:28:57 +0100

    --add: find next bin

--- app.vue
+++ app.vue
@@ -1,33 +1,70 @@
 <script setup lang="ts">
-const { data: binzData } = await useAsyncData('binz', () => $fetch('/api/binz'));
-
+import { isToday, isTomorrow, isFuture } from 'date-fns';
 const cols = {
   'Gelbe Tonne': 'yellow',
   Biotonne: 'green',
   Reststoff: 'gray',
   'Blaue Tonne': 'blue',
 };
+
+function createDate(str: string): Date {
+  const [day, month, year] = str.split('.');
+  return new Date(Number(year), Number(month) - 1, Number(day));
+}
+
+const { data: binzData } = await useAsyncData('binz', () => $fetch('/api/binz'));
 const isMounted = ref(false);
-const attributes = ref([]);
+const attributes = computed(() =>
+  binzData.value.map((binDay) => {
+    const [day, month, year] = binDay.date.split('.');
+    return {
+      key: binDay.date,
+      highlight: cols[binDay.bin],
+      dates: createDate(binDay.date),
+    };
+  }),
+);
+const nextBinComing = computed(() =>
+  binzData.value.find(
+    (binDay) => isToday(createDate(binDay.date)) || isTomorrow(createDate(binDay.date)),
+  ),
+);
+const nextBinComingColor = computed(() =>
+  nextBinComing.value ? nextBinComing.value.bin : 'white',
+);
+const nextBin = computed(() => binzData.value.find((binDay) => isFuture(createDate(binDay.date))));
+
 onMounted(() => {
   isMounted.value = true;
-  attributes.value = binzData.value.map((binDay, i) => {
-    const [day, month, year] = binDay.date.split('.');
-    const d = new Date(Number(year), Number(month) - 1, Number(day));
-    console.log('daym month, year: ', day, month, year);
-    console.log('d: ', d);
-    return {
-      key: i,
-      highlight: cols[binDay.bin],
-      dates: d,
-    };
-  });
 });
 </script>
 
 <template>
-  <div v-if="isMounted">
-    <v-calendar :attributes="attributes" />
-  </div>
-  <pre>{{ binzData }}</pre>
+  <main>
+    <h1 style="text-align: center">{{ nextBin.day }} - {{ nextBin.bin }}</h1>
+    <div v-if="isMounted" style="display: flex; justify-content: center">
+      <v-calendar :attributes="attributes" />
+    </div>
+  </main>
 </template>
+
+<style>
+html,
+body,
+main,
+#__nuxt {
+  height: 100%;
+}
+
+* {
+  margin: 0;
+}
+
+h1 {
+  padding: 24px 0;
+}
+
+main {
+  background-color: v-bind(nextBinComingColor);
+}
+</style>
