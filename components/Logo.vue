ea97eb6c54e1aa0ca5af7bcba9bdb826a0543d95
Peter Anderson <pete.anderson@gmail.com>
2022-01-04 20:01:17 +0100

    --refac: logo now dynamic

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,8 +1,18 @@
+<script setup lang="ts">
+const props = defineProps({
+  next4BinColors: {
+    type: Array,
+    default: ["#68d391", "#cbd5e0", "#63b3ed", "#f6e05e"]
+  }
+});
+</script>
+
 <template>
   <div class="logo-container">
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
+    <div
+      v-for="(color, i) in next4BinColors"
+      :key="i"
+      :style="`background-color: ${color}`"
+    ></div>
   </div>
 </template>
