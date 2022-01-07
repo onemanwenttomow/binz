45b6c0d93fb39d759c26c4db3bc9908594e169db
Peter Anderson <pete.anderson@gmail.com>
2022-01-07 14:01:24 +0100

    --add: bin lids

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
+const cssColors = {
+  '#f6e05e': 'yellow',
+  '#68d391': 'green',
+  '#cbd5e0': 'gray',
+  '#63b3ed': 'blue',
+};
 const props = defineProps({
   next4BinColors: {
     type: Array,
-    default: ["#68d391", "#cbd5e0", "#63b3ed", "#f6e05e"]
-  }
+    default: ['#68d391', '#cbd5e0', '#63b3ed', '#f6e05e'],
+  },
 });
 </script>
 
@@ -13,6 +19,9 @@ const props = defineProps({
       v-for="(color, i) in next4BinColors"
       :key="i"
       :style="`background-color: ${color}`"
-    ></div>
+      class="bin"
+    >
+      <div class="lid" :class="`${cssColors[color]}-lid`"></div>
+    </div>
   </div>
 </template>
