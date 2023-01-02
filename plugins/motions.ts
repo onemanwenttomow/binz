db7b73dcc0443b64cb2335ed7ed73124537473fe
Peter Anderson <pete.anderson@gmail.com>
2023-01-02 16:06:44 +0100

    --del: remove motionone

--- plugins/motions.ts
+++ plugins/motions.ts
@@ -1,5 +1,5 @@
-import { MotionOnePlugin } from 'vue-motion-one';
+// import { MotionOnePlugin } from 'vue-motion-one';
 
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(MotionOnePlugin);
-});
+// export default defineNuxtPlugin((nuxtApp) => {
+//   nuxtApp.vueApp.use(MotionOnePlugin);
+// });
